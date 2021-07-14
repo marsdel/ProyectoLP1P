74d8c8d5e0a2bc45590a22c0d60da342a7797c72
Marco Del Rosario <marsdel@espol.edu.ec>
2021-07-14 14:01:20 -0500

    modificacion en metodo de asignacion

--- sources/sintactico_semantico.py
+++ sources/sintactico_semantico.py
@@ -106,10 +106,13 @@ def p_args_method(p):
 
 def p_assignment(p):
     '''assignment : variable EQUAL_SYMBOL data
+                    | variable EQUAL_SYMBOL expression_operations
                     | array_data EQUAL_SYMBOL data
                     | method_invocation EQUAL_SYMBOL data 
                     | variable EQUAL_SYMBOL array
                     | variable EQUAL_SYMBOL hash
+                    | array_data EQUAL_SYMBOL expression_operations
+                    | method_invocation EQUAL_SYMBOL expression_operations
                     | self_assigment
                     | mult_assigment'''
     global resultado
